1e74d8f07cd8cb7dddb7c5e90065b694a8a9ce78
skorobogatovamail <skorobogatovamail@gmail.com>
2024-01-25 00:16:30 +0300

    add burger

--- js/main.js
+++ js/main.js
@@ -4,9 +4,22 @@
   window.onscroll = () => {
     if (window.scrollY > 150) {
       header.classList.add("header_active");
-    }
-    else {
+    } else {
       header.classList.remove("header_active");
     }
   };
+
+  const headerBurger = document.querySelector(".header__burger");
+  const headerNav = document.querySelector(".header__nav");
+  const headerNavClose = document.querySelector(".header__nav-close");
+
+  headerBurger.addEventListener("click", () => {
+    console.log('clicked headerBurger');
+    headerNav.classList.add("header__nav-active");
+  });
+
+  headerNavClose.addEventListener("click", () => {
+    console.log('clicked headerNavClose');
+    headerNav.classList.remove("header__nav-active");
+  });
 })();
